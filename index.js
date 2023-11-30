fb0d1ae5043ee0986248407c8d7f45d7f7c6d499
Mikeoliveira07 <joaomikeoliveira@gmail.com>
2023-11-29 22:55:06 -0300

    feat: create port

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const nome = "Mike";
+const express = require("express");
+const app = express();
 
-console.log(nome);
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.info("Aplicação rodando em http://localhost:3000");
+});
+
