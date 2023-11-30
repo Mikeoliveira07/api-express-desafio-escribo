af81138bfedafdde3bc70a8d716253d4255d9c1c
Mikeoliveira07 <joaomikeoliveira@gmail.com>
2023-11-29 23:39:17 -0300

    feat: http

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,18 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.send("Login");
+})
+
+http.get('http://localhost:3000/controllers/userController', (res) => {
+  let data = ''
+
+  res.on('data', (chunk) => {
+    data += chunk
+  })
+
+  res.on('end', () => {
+    console.log(data)
+  })
 })
 
 app.listen(port, () => {
