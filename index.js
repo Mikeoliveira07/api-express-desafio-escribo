cb7b9f22003e74c512439616b4531db628c158d7
Mikeoliveira07 <joaomikeoliveira@gmail.com>
2023-11-29 23:28:06 -0300

    feat: login

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    router.get('/user', authMiddleware, userController.getUser);
+    res.send("Hello World!");
 });
+
+app.get("/login", (req, res) => {
+    res.send("Login");
+})
 
 app.listen(port, () => {
     console.info("Aplicação rodando em http://localhost:3000");
